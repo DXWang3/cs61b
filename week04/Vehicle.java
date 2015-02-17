6d51529cacde2b94db5a7c566a8e739c15d7eb96
Sammy Sidhu <sammmy.sidhu@gmail.com>
2015-02-16 20:14:39 -0800

    added questions for Vehicle

--- week04/Vehicle.java
+++ week04/Vehicle.java
@@ -24,27 +24,12 @@ public abstract class Vehicle {
     float range() {
         return fuel * mpg;
     }
-
-    public static void main(String[] args) {
-
-        Vehicle v1 = new Vehicle(3,4,20,10);
-        System.out.println("Range of v1: " + v1.range());
-
-
-        Vehicle v2 = new Car(20,20);
-        System.out.println(v2.range());
-
-        Vehicle v3 = new Truck();
-        
-
-    }
-    
 }
 
 class Car extends Vehicle {
     public Car(int fuel, int mpg) {
         super(4, 4, fuel, mpg); 
-        this.trunkSize = 5;
+        this.trunkSize = 2;
     }      
 
     public void putInTrunk(Object item) {
@@ -63,15 +48,41 @@ class Motorcycle extends Vehicle {
     }   
 }
 
+/* Fill this class in assuming that the trunkSize of a Truck is 5*/
+public class Truck extends Car {
+     public Truck() {
 
-// public class Truck extends Car {
-//     public Truck() {
-
-//     }
-// }
-
-
+     }
+}
 
 
+What will happen after each of these snippets of code are compiled/run?
+        //Q1
+    Vehicle v1 = new Vehicle(3,4,20,10);
+    System.out.println("Range of v1: " + v1.range());
 
+    //Q2
+    Vehicle v2 = new Car(20,20);
+    System.out.println("Range of v2: " + v2.range());
 
+    //Q3
+    Vehicle v3 = new Motorcycle(10,40);
+    System.out.println("Range of v3: " + v3.range());
+    
+    //Q4
+    System.out.println("Number of seats of v2 " + v2.seats);
+    System.out.println("Number of seats of v3 " + v3.seats);
+
+    //Q5
+    System.out.println("Number of wheels of v2" + v2.wheels);
+    System.out.println("Number of wheels of v3" + v3.wheels);
+
+    //Q6
+    v2.putInTrunk("Backpack");
+    v2.putInTrunk("Laptop");
+    v2.putInTrunk("Shoes");
+
+    //Q7
+    v3.putInTrunk("Backpack");
+    v3.putInTrunk("Laptop");
+    v3.putInTrunk("Shoes");
